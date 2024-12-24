76d3040e0883ce715f4d3d2f16ac70828fc6c528
Abhishek Kumar Sharma <akankshasharma47380@gmail.com>
2024-12-25 00:39:38 +0530

    Update header text for improved clarity

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
    return (
       <div className="header">
          <div className="header-contents">
-            <h2>Order Your Best Food Here </h2>
+            <h2>Order Now </h2>
             <p>
                Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.
                Our mission is to satisfy your cravings and elevate your dining experience..
