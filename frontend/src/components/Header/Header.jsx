96530f4bdd164fa4238dfa5dd244a5c8c8be0649
Abhishek Kumar Sharma <akankshasharma47380@gmail.com>
2024-12-25 00:37:44 +0530

    Refactor header text for clarity and conciseness

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,20 +2,18 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="header-contents">
-        <h2>Order your favourite food here </h2>
-        <p>
-          Choose from a diverse menu featuring a delectable array of dishes
-          crafted with the finest ingredients and culinary expertise. Our
-          mission is to satisfy your cravings and elevate your dining
-          experience, one delicious meal at a time.
-        </p>
-        <button>View Menu</button>
+   return (
+      <div className="header">
+         <div className="header-contents">
+            <h2>Order Your Best Food Here </h2>
+            <p>
+               Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise.
+               Our mission is to satisfy your cravings and elevate your dining experience..
+            </p>
+            <button>View Menu</button>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Header;
